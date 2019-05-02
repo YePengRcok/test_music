87f8efb5df68efcaca47f02bca9c3631ffd293d0
YePengPcok <3170764607@qq.com>
2019-05-02 14:48:21 +0800

    视屏播放和音乐播放的小原理

--- 客户端.cpp/kehu.cpp
+++ 客户端.cpp/kehu.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
 #include <graphics.h>
-#include <conio.h>
+#include <stdio.h>
+#include <MMSystem.h>
+#pragma comment(lib,"winmm.lib")
+#define COUNT 148
 
+using namespace std;
 int main(void) {
-	char pwd[7];
-	char dict[64];
-	char tmp[32];
-	int index = 0;
-	for(int i = 0; i < 10; i++) {
-		dict[index++] = '0' + i;
+	char jile_name[128];
+	IMAGE jile_image[COUNT];
+	cout << "正在加载..." << endl;
+
+	for(int i = 1; i <= COUNT; i++) {
+		sprintf(jile_name, "E:\\客户端.cpp\\客户端.cpp\\images2\\_%04d_图层-%d.jpg" , 148 - i, i);
+		loadimage(&jile_image[i - 1], jile_name);
 	}
 
-	int n = index;
-	for(int p1 = 0; p1 < n; p1++) {
-		for(int p2 = 0; p2 < n; p2++) {
-			for(int p3 = 0; p3 < n; p3++) {
-				for(int p4 = 0; p4 < n; p4++) {
-					for(int p5 = 0; p5 < n; p5++) {
-						for(int p6 = 0; p6 < n; p6++) {
-							tmp[0] = dict[p1];
-							tmp[1] = dict[p2];
-							tmp[2] = dict[p3];
-							tmp[3] = dict[p4];
-							tmp[4] = dict[p5];
-							tmp[5] = dict[p6];
-							tmp[6] = '\0';
-							std::cout << tmp << std::endl;
-							Sleep(10);
-							
-						}
-					}
-				}
-			}
+
+	initgraph(800, 450);
+	mciSendString(_T("play E:\\客户端.cpp\\客户端.cpp\\极乐净土.mp3 repeat"), 0, 0, 0);
+	
+	while(1) {
+		for(int i = 0; i < COUNT; i++) {
+			putimage(0, 0, &jile_image[i]);
+			Sleep(70);
 		}
 	}
+
 	system("pause");
 	return 0;
 }
